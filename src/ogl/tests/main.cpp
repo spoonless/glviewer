5d8665e99546b37d2de58a3ea6401425e60a24d9
David Gayerie <david1.gayerie@orange.com>
2015-05-15 01:31:03 +0200

    using multimodules

---
+++ src/ogl/tests/main.cpp
@@ -0,0 +1,13 @@
+#include "gtest/gtest.h"
+#include "GlWindowContext.hpp"
+
+int main(int argc, char **argv) {
+    gl::GlWindowContext glwc;
+    if(!glwc.init("unitttest", 1, 1) || !glwc.makeCurrent())
+    {
+        return 1;
+    }
+
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
