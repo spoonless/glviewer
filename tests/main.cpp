2a4d1cd824de15e2c73efd6f45608c4f29ce95bf
David Gayerie <david1.gayerie@orange.com>
2015-05-14 02:06:47 +0200

    uncomment OpenGL init for unittests

--- tests/main.cpp
+++ tests/main.cpp
@@ -4,13 +4,11 @@
 #include "GlWindowContext.hpp"
 
 int main(int argc, char **argv) {
-	/*
     gl::GlWindowContext glwc;
     if(!glwc.init("unitttest", 1, 1) || !glwc.makeCurrent())
     {
         return 1;
     }
-	*/
 
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
