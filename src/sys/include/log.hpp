a17229ef4ff229ff1ea779567251c228f39875d2
David Gayerie <dagaydevel@free.fr>
2015-05-20 19:31:13 +0200

    modifying g3log obfuscation. Global unique_ptr prevented MSVC exe to exit normally

--- src/sys/include/log.hpp
+++ src/sys/include/log.hpp
@@ -1,6 +1,7 @@
 #ifndef LOG_HPP
 #define LOG_HPP
 
+#include <memory>
 #include "config.h"
 
 #ifdef USE_G3LOG
@@ -9,29 +10,46 @@
 
 #else
 
-#include <ostream>
 #include <cassert>
+#include <ostream>
 
 #define DEBUG 0
 #define INFO 0
 #define WARNING 0
 #define FATAL 0
 
+namespace sys
+{
+
 namespace ignore
 {
     extern std::ostream nullostream;
 }
 
-#define LOG(level) if(level) ignore::nullostream
-#define LOGF(level, printf_like_message, ...) if(level) assert(1)
+}
+
+#define LOG(level) if(false) sys::ignore::nullostream
+#define LOGF(level, printf_like_message, ...) if(false) assert(1)
 #define CHECK(x) assert(x)
 
 #endif
 
-
 namespace sys
 {
-    void initLogger();
+    class LoggingSystem
+    {
+    public:
+        LoggingSystem(const LoggingSystem &) = delete;
+        LoggingSystem & operator = (const LoggingSystem &) = delete;
+        virtual ~LoggingSystem();
+
+        static std::unique_ptr<LoggingSystem> create();
+
+    protected:
+        LoggingSystem();
+    };
 }
+
+#define INIT_LOGGING_SYSTEM() auto loggingSystem = sys::LoggingSystem::create(); ((void*) &loggingSystem)
 
 #endif
